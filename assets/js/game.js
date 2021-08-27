70f3f798f1578b37966111ca63eb214edd22c4b9
kvammee <elliott.kvamme@gmail.com>
2021-08-27 14:58:31 -0500

    Added high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -117,12 +117,28 @@ var startGame = function() {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-  } else {
-    window.alert("You've lost your robot in battle!");
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
   }
+
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+  // if player is still alive, player wins!
+  //if (playerInfo.health > 0) {
+  //  window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
+
+  //} else {
+  //  window.alert("You've lost your robot in battle!");
+  //}
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
@@ -194,7 +210,7 @@ var fightOrSkip = function() {
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
-      playerInfo.money = playerInfo.moeny - 10;
+      playerInfo.money = playerInfo.money - 10;
       
       // return true if the player wants to leave
       return true;
